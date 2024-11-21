36ac4f82cc06145d15c56b34ff644dc0637caaa5
尘少 <1982740368@qq.com>
2024-11-21 20:30:05 +0800

    feat: 新增stylelint

---
+++ packages/stylelint-config/README.md
@@ -0,0 +1,23 @@
+# `luofeng-stylelint-config`
+
+## 参考
+
+[eslint中文官网](https://www.stylelint.cn/)
+
+## 开发此包
+
+### 看项目是否要使用Sass?
+
+[stylelint-scss](https://www.npmjs.com/package/stylelint-scss)
+
+书写规则index.js
+
+## 使用
+
+### 在 `.stylelintrc` 中继承本包
+
+```json
+{
+  "extends": "子包名"
+}
+```
---
+++ packages/stylelint-config/__tests__/stylelint-config.test.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+const stylelint = require('stylelint');
+const path = require('path');
+
+describe('测试一组css文件', () => {
+  it('测试错误css', async () => {
+    const filePaths = [path.join(__dirname, '../fixtures/error.css')];
+    const result = await stylelint.lint({
+      configFile: path.join(__dirname, '../index.js'),
+      files: filePaths,
+      fix: false,
+    });
+    if (result && result.errored) {
+      const filesResult = JSON.parse(result.output || '[]') || [];
+      filesResult.forEach((fileResult) => {
+        console.log(`========= ${filePaths} ==========`);
+        console.log(fileResult.warnings);
+      });
+      assert.ok(filesResult.length !== 0);
+    }
+  })
+})
